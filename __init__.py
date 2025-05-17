4d81778476674d5e0305d1cb197d69293e11163c
Alreco478 <91104588+Alreco478@users.noreply.github.com>
2025-05-17 11:43:33 -0700

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-
-from explore import summarize_df, check_missing_values
-from transform import filter_to_plays, aggregate_df, add_calc_stats
-from model import linreg_evaluate, rf_model_evaluate, logreg_model_evaluate
-from predict import predict_winner, predict_week
-from optimize.py import optimize_year, optimize_alpha
